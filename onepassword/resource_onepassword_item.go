04b784e605df7e9f3baee87578ede09f2cd4e070
Scott Doyland <scott.doyland@hogarth.com>
2024-03-28 15:50:50 +0000

    sort tags in alphabetical order before storing in terraform

--- onepassword/resource_onepassword_item.go
+++ onepassword/resource_onepassword_item.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"regexp"
+	"sort"
 	"strings"
 
 	"github.com/1Password/connect-sdk-go/onepassword"
@@ -364,7 +365,14 @@ func itemToData(item *onepassword.Item, data *schema.ResourceData) error {
 		}
 	}
 
-	data.Set("tags", item.Tags)
+	// Create a copy of the tags slice to avoid modifying the original slice
+	sortedTags := make([]string, len(item.Tags))
+	copy(sortedTags, item.Tags)
+
+	// Sort and set the tags in alphabetical order
+	sort.Strings(sortedTags)
+	data.Set("tags", sortedTags)
+
 	data.Set("category", strings.ToLower(string(item.Category)))
 
 	dataSections := data.Get("section").([]interface{})
